7e31b971dd6db285e7d4e18ccb0541633562c7ba
Maximilian Hammerl <maximilian@hammerl.dev>
2024-12-04 10:51:25 +0100

    Measure time

--- src/AdventOfCodeChallenge.kt
+++ src/AdventOfCodeChallenge.kt
@@ -1,6 +1,7 @@
 import java.io.BufferedReader
 import java.io.File
 import java.io.InputStream
+import kotlin.time.measureTimedValue
 
 abstract class AdventOfCodeChallenge<FirstTaskResult, SecondTaskResult>(
     private val day: Int,
@@ -13,14 +14,23 @@ abstract class AdventOfCodeChallenge<FirstTaskResult, SecondTaskResult>(
     private fun validateFirstTask(
         expectedFirstTaskResult: FirstTaskResult,
     ) {
-        val actualFirstTaskResult =  solveFirstTask()
-        check(actualFirstTaskResult != null && expectedFirstTaskResult != null && actualFirstTaskResult == expectedFirstTaskResult) { "First task: Actual $actualFirstTaskResult not equal to expected $expectedFirstTaskResult"}
+        val (actualFirstTaskResult, timeTaken) = measureTimedValue {
+            solveFirstTask()
+        }
+
+        println("Day $day, task 1: $timeTaken")
+
+        check(actualFirstTaskResult != null && expectedFirstTaskResult != null && actualFirstTaskResult == expectedFirstTaskResult) { "First task: Actual $actualFirstTaskResult not equal to expected $expectedFirstTaskResult" }
     }
 
     private fun validateSecondTask(expectedSecondTaskResult: SecondTaskResult) {
-        val actualSecondTaskResult =  solveSecondTask()
-        check(actualSecondTaskResult != null && expectedSecondTaskResult != null && actualSecondTaskResult == expectedSecondTaskResult) { "Second task: Actual $actualSecondTaskResult not equal to expected $expectedSecondTaskResult"}
+        val (actualSecondTaskResult, timeTaken) = measureTimedValue {
+            solveSecondTask()
+        }
 
+        println("Day $day, task 2: $timeTaken")
+
+        check(actualSecondTaskResult != null && expectedSecondTaskResult != null && actualSecondTaskResult == expectedSecondTaskResult) { "Second task: Actual $actualSecondTaskResult not equal to expected $expectedSecondTaskResult" }
     }
 
     fun validate(
--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,6 @@
 fun main() {
-    // Day01().validate(2086478, 24941624)
-    // Day02().validate(559, 601)
-    // Day03().validate(182780583, 90772405)
+    Day01().validate(2086478, 24941624)
+    Day02().validate(559, 601)
+    Day03().validate(182780583, 90772405)
     Day04().validate(2496, 1967)
 }
