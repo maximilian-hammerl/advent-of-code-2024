d78018fdfb2a8928c6648eb85ff00b888e585990
Maximilian Hammerl <maximilian@hammerl.dev>
2024-12-04 08:30:45 +0100

    Day 3

--- src/Day03.kt
+++ src/Day03.kt
@@ -22,24 +22,22 @@ class Day03 : AdventOfCodeChallenge<Int, Int>(3) {
         val regex = """mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)""".toRegex()
 
         val input = readInput().readLines().joinToString("")
-
-        val matches = regex.findAll(input)
-
         var result = 0
 
         var doMultiply = true
 
-        for (match in matches) {
-            println("Found match: ${match.value}")
-            if (match.value.startsWith("don't")) {
-                doMultiply = false
-            } else if (match.value.startsWith("do")) {
-                doMultiply = true
-            } else if (match.value.startsWith("mul") && doMultiply) {
-                val x = match.groupValues[1].toInt()
-                val y = match.groupValues[2].toInt()
+        regex.findAll(input).forEach { match ->
+            when (match.value) {
+                "don't()" -> doMultiply = false
+                "do()" -> doMultiply = true
+                else -> {
+                    if (doMultiply) {
+                        val x = match.groupValues[1].toInt()
+                        val y = match.groupValues[2].toInt()
 
-                result += (x * y)
+                        result += (x * y)
+                    }
+                }
             }
         }
 
